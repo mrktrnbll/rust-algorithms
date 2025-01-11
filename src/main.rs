2d419c3c751a325d44bfd6af435144d3317a6f09
Mark Turnbull <mrktrnbll@gmail.com>
2025-01-11 00:05:20 +0000

    adds functions to read in space delimited text files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
+fn read_file(filename: &str) -> String {
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    return contents;
+}
+
+fn string_to_array(string: String) -> Vec<i32> {
+    let numbers = string.split(" ").map(|number| number.parse::<i32>().unwrap()).map(|number| number).collect();
+
+    return numbers;
+}
+
 fn main() {
     println!("Hello, world!");
+
+    println!("The file contains: {}", read_file("data/100.txt"));
+    let string = read_file("data/100.txt");
+    let array = string_to_array(string);
+    println!("{:?}", array);
 }
